0354c7e3b9955390360e157e955cf950935b95b3
Roman Ovchinnikov <coolthecold@gmail.com>
2012-04-10 00:15:47 +0400

    addnat now reads config as well

--- openvz/vzcreate/addnat.sh
+++ openvz/vzcreate/addnat.sh
@@ -4,8 +4,8 @@ vzconfig=vzcreate.conf
 ARGS=1
 if [[ $# -ne "$ARGS"  ]]
 then
-    echo "usage: `basename $0` <ipaddress>>"
-    echo "for example: `basename $0` 10.10.10.5"
+    echo "usage: `basename $0` <VE ipaddress in iptables format>"
+    echo "for example: `basename $0` 10.10.10.5/32"
     exit 1
 
 fi
@@ -16,14 +16,9 @@ else
     echo "failed to read config $vzconfig"
     exit 2
 fi
-nsstring=""
-for i in $nameservers;do
-    #echo "nameserver is $i"
-    nsstring="$nsstring --nameserver $i"
-done
 #echo "my ip is $myip"
 
-if [ -z "$nsstring" ];then echo "nameservers list is empty!";exit 2;fi
 if [ -z "$myip" ];then echo "myip is empty!";exit 2;fi
+if [ -z "$GINIF" ];then echo "GINIF is empty!";exit 2;fi
 	    
-iptables -t nat -A POSTROUTING -s $1 -o eth0 -j SNAT --to $myip
+iptables -t nat -A POSTROUTING -s $1 -o $GINIF -j SNAT --to $myip
